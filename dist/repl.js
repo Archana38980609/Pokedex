1d4b6b12973f8f593ece1bd48edf648cc27e1055
Archana38980609 <archi.dtg@gmail.com>
2025-08-07 16:30:30 +0000

    Add simple REPL that shows prompt, cleans input, and prints first word

---
+++ dist/repl.js
@@ -0,0 +1,25 @@
+import { createInterface } from "node:readline";
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > ",
+    });
+    rl.prompt();
+    rl.on("line", (input) => {
+        const words = cleanInput(input);
+        if (words.length === 0) {
+            rl.prompt();
+            return;
+        }
+        console.log(`Your command was: ${words[0]}`);
+        rl.prompt();
+    });
+}
+export function cleanInput(input) {
+    const lowerCased = input.toLowerCase();
+    const stripped = lowerCased.trim();
+    const splitted = stripped.split(" ");
+    const filteredArray = splitted.filter(str => str !== "");
+    return filteredArray;
+}
