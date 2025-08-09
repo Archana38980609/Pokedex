a5cdd00a757ab344cc7dacb01691e50c0ba7788c
Archana38980609 <archi.dtg@gmail.com>
2025-08-09 06:12:25 +0000

    Add map and mapb commands to browse Pokemon locations page by page; update State and REPL to handle async commands

--- src/command_help.ts
+++ src/command_help.ts
@@ -1,5 +1,5 @@
 import type { State } from "./state.js"
-export function commandHelp(state : State){
+export async function commandHelp(state : State){
 console.log();
 console.log("Welcome to the Pokedex!");
 console.log("Usage:");
